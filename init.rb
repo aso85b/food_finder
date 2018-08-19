c62b67656e401376cf441a3e16ed2688ba6c92a1
Aso <aso85b@gmail.com>
2018-08-19 21:36:19 +0430

    start

---
+++ init.rb
@@ -0,0 +1,13 @@
+#### Food Finder ####
+#
+#Launch this Ruby file from the command line
+#to get started
+#
+
+APP_ROOT = File.dirname(__FILE__)
+#require  "#{APP_ROOT}/lib/guide"
+#reqire File.join(APP_ROOT, 'lib', 'guide.rb')
+#reqire File.join(APP_ROOT, 'lib', 'guide')
+
+$: .unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
