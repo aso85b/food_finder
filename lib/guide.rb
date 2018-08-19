9675b0cc53ecf57b06217895aaab15509c9a0161
Aso <aso85b@gmail.com>
2018-08-19 23:37:02 +0430

    do changes on guide class

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,11 +1,24 @@
+require "restaurant"
 class Guide 
 
+#-------------------------------------------
 def initialize(path=nil)
+
  #locate the restaurant text file at path
+ Restaurant.filepath = path
+
  #or create a new file
  #exit if create fails
+ if Restaurant.file_exists?
+ 	puts "Found restaurant file."
+ elsif Restaurant.create_file
+ 	puts "Created restaurant file."
+ else
+ 	puts "Exiting...\n\n"
+ 	exit! #program exits anyway!
+ end
 end
-
+#-------------------------------------------
 def launch!
 	introduction
 	#action loop
@@ -16,14 +29,15 @@ def launch!
 
 end
 
+#-------------------------------------------
 def introduction
  puts "\n\n<<< Welcome to the Food Finder Program >>>\n\n"
  puts "This is an interactive guide to help you find the food you crave.\n\n"
 end
 
-
+#-------------------------------------------
 def conclusion
 	puts "\n<<< Goodbye an Bon Appetit! >>>\n\n\n"
 end
-
+#-------------------------------------------
 end
